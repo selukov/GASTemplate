bfee7af3bce5a08a57dbc9043ff483b0ba9d831d
andrew <selukov.andrey@gmail.com>
2024-01-09 20:33:37 +0700

    Initialize Project Setup and Configuration

---
+++ README.md
@@ -0,0 +1,14 @@
+# This is a template for Google Apps Script projects.
+
+## To get started, execute the following commands.
+
+– retrieves the ~/.clasprc.json file with the key.
+   ```bash
+   npm run glogin
+   ```
+– creates a project; in the console, select 'form'.
+
+   ```bash
+   npm run gcreate
+   ```
+After executing the gcreate command, you will find files in the 'src' directory. Copy the .clasp.json file to the project's root.
--- src/api/index.js
+++ src/api/index.js
@@ -3,54 +3,54 @@ const slideTemplateId = "PRESENTATION_ID"; // "PRESENTATION_ID";
 const key = "KEY-ID";
 const md5sum = "7b70bd59884a8fb0e8b6ea3c3daac46e";
 const fileUrl =
-    "https://drive.google.com/uc?export=download&id=PRESENTATION_ID";
+  "https://drive.google.com/uc?export=download&id=PRESENTATION_ID";
 
 function setFields() {
-    const apiUrl = "http://Localhost:8085/api/v2/fill";
+  const apiUrl = "http://Localhost:8085/api/v2/fill";
 
-    const data = {
-        Name: "John",
-        Address: "My address2",
-    };
+  const data = {
+    Name: "John",
+    Address: "My address2",
+  };
 
-    const payload = {
-        method: "post",
-        contentType: "application/x-www-form-urlencoded",
-        muteHttpExceptions: true,
-        payload: {
-            data: JSON.stringify(data),
-            fileUrl: fileUrl,
-            md5sum: md5sum,
-            key: key,
-        },
-    };
+  const payload = {
+    method: "post",
+    contentType: "application/x-www-form-urlencoded",
+    muteHttpExceptions: true,
+    payload: {
+      data: JSON.stringify(data),
+      fileUrl: fileUrl,
+      md5sum: md5sum,
+      key: key,
+    },
+  };
 
-    const response = UrlFetchApp.fetch(apiUrl, payload);
+  const response = UrlFetchApp.fetch(apiUrl, payload);
 
-    // Обрабатываем ответ сервера
-    const responseData = response.getContentText();
-    Logger.log(responseData);
+  // Обрабатываем ответ сервера
+  const responseData = response.getContentText();
+  Logger.log(responseData);
 }
 
 function getFields() {
-    const apiUrl = "http://Localhost:8085/api/v2/fields";
+  const apiUrl = "http://Localhost:8085/api/v2/fields";
 
-    const encodedUrl = encodeURIComponent(fileUrl);
+  const encodedUrl = encodeURIComponent(fileUrl);
 
-    // Конструируем URL с параметрами
-    const urlWithParams = `${apiUrl}?fileUrl=${encodedUrl}&md5sum=${md5sum}&key=${key}`;
+  // Конструируем URL с параметрами
+  const urlWithParams = `${apiUrl}?fileUrl=${encodedUrl}&md5sum=${md5sum}&key=${key}`;
 
-    Logger.log(urlWithParams);
+  Logger.log(urlWithParams);
 
-    const params = {
-        method: "get",
-        muteHttpExceptions: true, // Для отладки, чтобы не подавлять исключения HTTP
-    };
+  const params = {
+    method: "get",
+    muteHttpExceptions: true, // Для отладки, чтобы не подавлять исключения HTTP
+  };
 
-    // Делаем GET-запрос
-    const response = UrlFetchApp.fetch(urlWithParams, params);
+  // Делаем GET-запрос
+  const response = UrlFetchApp.fetch(urlWithParams, params);
 
-    // Обрабатываем ответ сервера
-    const responseData = response.getContentText();
-    Logger.log(responseData);
+  // Обрабатываем ответ сервера
+  const responseData = response.getContentText();
+  Logger.log(responseData);
 }
